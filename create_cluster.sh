49ce6fb861262e6ee582604d755d6363d318e898
Andrey Bagrintsev <andrey@bagrintsev.me>
2017-07-17 23:57:45 +0300

    create_cluster.sh fix, extended finish message, composer names fix

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,13 +1,16 @@
+#!/bin/bash
+
 #install redis tools (for deb based linux)
 apt-get update && apt-get install ruby wget redis-tools -y
 
 #download and install redis cluster tool
-wget http://download.redis.io/redis-stable/src/redis-trib.rb
+wget http://download.redis.io/redis-stable/src/redis-trib.rb -O ./redis-trib.rb
 chmod +x redis-trib.rb
 gem install redis
 
-./redis-trib.rb create --replicas 1 $(echo $(for i in {1..6}; do docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' rediscluster_node-$i\_1; done  | sed ':a;N;$!ba;s/\n/:6379 /g'):6379)
+./redis-trib.rb create --replicas 1 $(echo $(for i in {1..6}; do docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' rediscluster_node-$i\_1; done | sed ':a;N;$!ba;s/\n/:6379 /g'):6379)
 
-echo run
-echo redis-cli -c -h <ANY NODE IP> CLUSTER INFO
-echo for info
+echo -e "\n \t \e[32m \033[1m ALL DONE! \033[0m \n"
+echo 'Please, run:'
+echo '`redis-cli -c -h <ANY YOUR DOCKER CLUSTER IP> CLUSTER INFO`'
+echo 'to read information about the assembled cluster'
